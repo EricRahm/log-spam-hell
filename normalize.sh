a6591d985dc97807320ebad461e776f9903e6196
Eric Rahm <ericrahm@gmail.com>
2015-06-29 15:56:58 -0700

    Support both GNU and BSD sed

--- normalize.sh
+++ normalize.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Deal w/ BSD sed vs GNU sed
+case "$(uname -s)" in
+  Darwin|*BSD)
+    SED='sed -i "" '
+    ;;
+
+  Linux)
+    SED='sed -i '
+    ;;
+
+  *)
+    echo "untested platform"
+    SED='sed -i '
+    ;;
+esac
+
 # Normalizes log files to make collation of warnings easier
 # - Remove timestamp stuff, ie "09:54:12   INFO - "
 # - Remove hex addresses/values, ie: "0x7fccc5bbd2f0"
@@ -8,13 +24,14 @@
 normalize_file()
 {
   file=$1
-  sed -i "" -E -e 's/^[0-9:]+[[:space:]]+INFO[[:space:]]+-[[:space:]]+//g'        \
-               -e 's/\[(Child|Parent|GMP|NPAPI)?[[:space:]]?[0-9]+\]/\[NNNNN\]/g' \
-               -e 's/file (\/builds\/.*\/build\/src\/)([^[:space:]]+)/file \2/g'  \
-               -e 's/file (\/builds\/.*\/build\/gecko\/)([^[:space:]]+)/file \2/g'  \
-               -e 's/0x[0-9a-fA-F]+/0xNNNNNNNN/g' $file
+  $SED -E -e 's/^[0-9:]+[[:space:]]+INFO[[:space:]]+-[[:space:]]+//g'        \
+          -e 's/\[(Child|Parent|GMP|NPAPI)?[[:space:]]?[0-9]+\]/\[NNNNN\]/g' \
+          -e 's/file (\/builds\/.*\/build\/src\/)([^[:space:]]+)/file \2/g'  \
+          -e 's/file (\/builds\/.*\/build\/gecko\/)([^[:space:]]+)/file \2/g'  \
+	  $file
 }
 
 for file in "$@"; do
+  echo "normalizing file: $file"
   normalize_file "$file"
 done

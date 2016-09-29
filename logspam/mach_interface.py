78411b62324d5a79aa1b15895f86a8d4d1dd80ef
Eric Rahm <ericrahm@gmail.com>
2016-09-29 16:28:08 -0700

    Prep for release to pypi

---
+++ logspam/mach_interface.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+"""
+This module provides an API to integrate logspam within mach.
+
+Be careful to not break backward compatibility here, or you will
+break mach!
+"""
+
+from argparse import (ArgumentParser, Namespace)
+
+from logspam import __version__
+
+from logspam.bisect import BisectCommandLineArgs
+from logspam.bugzilla import FileCommandLineArgs
+from logspam.report import ReportCommandLineArgs
+
+import requests
+
+def new_release_on_pypi():
+    """
+    Check if a new release is available on pypi and returns it.
+
+    None is returned in case of error or if there is no new version.
+    """
+    return None
+    try:
+        #url = "https://pypi.python.org/pypi/logspam/json"
+        url = "https://testpypi.python.org/pypi/mozilla-log-spam/json"
+        pypi_version = requests.get(url, timeout=10).json()['info']['version']
+    except Exception:
+        return
+    if pypi_version != __version__:
+        return pypi_version
+
+
+def parser(subcommand=None):
+    """
+    Create and returns the logspam ArgumentParser instance.
+    """
+    p = ArgumentParser()
+
+    if not subcommand:
+        subparsers = p.add_subparsers(title='subcommands',
+                                      description='valid subcommands',
+                                      help='additional help')
+
+        for command in (ReportCommandLineArgs, FileCommandLineArgs, BisectCommandLineArgs):
+            args = command()
+            args.add_command(subparsers)
+    else:
+        ARG_HANDLERS = {
+            "report": ReportCommandLineArgs,
+            "file": FileCommandLineArgs,
+            "bisect": BisectCommandLineArgs,
+        }
+
+        ARG_HANDLERS[subcommand]().add_arguments(p)
+
+    return p
+
+
+def run(options):
+    """
+    Run logspam given a dict of options.
+    """
+    options['func'](options)
--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ from setuptools import setup
 
 setup(
     name="mozilla-log-spam",
-    version="0.0.1",
+    version="0.1",
     description="Mozilla test log spam classifier",
     long_description="Tools for identifying the most verbose warnings emitted during testing.",
     url="https://github.com/EricRahm/log-spam-hell",
     author="Eric Rahm",
     author_email="erahm@mozilla.com",
+    url="https://github.com/EricRahm/log-spam-hell",
+    download_url="https://github.com/EricRahm/log-spam-hell/tarball/0.1",
     license="MPL 2.0",
     classifiers=[
       "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"
     ],
-    #packages=["atsy"],
+    packages=["logspam"],
     install_requires=[
       "mozregression",
       "requests",

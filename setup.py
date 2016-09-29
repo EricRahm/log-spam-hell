c8063820ad5240e4a52729cd8c290c879ebf807b
Eric Rahm <ericrahm@gmail.com>
2016-09-29 16:48:52 -0700

    Update to 0.1.1

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import setup
 
 setup(
     name="mozilla-log-spam",
-    version="0.1",
+    version="0.1.1",
     description="Mozilla test log spam classifier",
     long_description="Tools for identifying the most verbose warnings emitted during testing.",
     url="https://github.com/EricRahm/log-spam-hell",
     author="Eric Rahm",
     author_email="erahm@mozilla.com",
-    download_url="https://github.com/EricRahm/log-spam-hell/tarball/0.1",
+    download_url="https://github.com/EricRahm/log-spam-hell/tarball/0.1.1",
     license="MPL 2.0",
     classifiers=[
       "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"

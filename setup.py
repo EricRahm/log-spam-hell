32fc04ece006a514c91ced9c6a3a3e571eaa4e52
Eric Rahm <ericrahm@gmail.com>
2016-09-29 16:57:15 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from setuptools import setup
+from logspam import __version__
 
 setup(
     name="mozilla-log-spam",
-    version="0.1.1",
+    version=__version__,
     description="Mozilla test log spam classifier",
     long_description="Tools for identifying the most verbose warnings emitted during testing.",
     url="https://github.com/EricRahm/log-spam-hell",
     author="Eric Rahm",
     author_email="erahm@mozilla.com",
-    download_url="https://github.com/EricRahm/log-spam-hell/tarball/0.1.1",
+    download_url="https://github.com/EricRahm/log-spam-hell/tarball/%s" % __version__,
     license="MPL 2.0",
     classifiers=[
       "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"
